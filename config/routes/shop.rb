c2895c783bcf084174700646b759cde116ac2832
Ashcon Partovi <Electroid@users.noreply.github.com>
2017-09-10 11:06:35 -0700

    Fix redirect

--- config/routes/shop.rb
+++ config/routes/shop.rb
@@ -1,5 +1,5 @@
 PGM::Application.routes.draw do
-	get  '/shop', :to => 'http://shop.stratus.network'
+	get '/shop' => redirect('http://shop.stratus.network')
 	# post '/shop/status', :to => 'shop#status'
 	# post '/shop/purchase', :to => 'shop#purchase'
 	# get  '/shop/thanks', :to => 'shop#thanks'
