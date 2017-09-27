1aa1664e9957f21f77cbb7fafc86cc1cd2b17cec
Matic0B <Matic0B@users.noreply.github.com>
2017-09-27 19:16:19 +0200

    Get the theme menu working

--- app/models/user/themes.rb
+++ app/models/user/themes.rb
@@ -3,8 +3,12 @@ class User
         extend ActiveSupport::Concern
 
         included do
-            field :web_theme, type: String, default: Default
+            field :web_theme, type: String
             attr_accessible :web_theme, as: :user
+        end
+
+        def dark_theme?
+            web_theme == "Dark Theme"
         end
     end
 end
