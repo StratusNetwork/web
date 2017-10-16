d18bb431b8bf7f634ff900973a370428e44c1172
Matic0B <matic0basle@gmail.com>
2017-10-16 16:51:26 +0200

    Themes out of Beta & Dyslexia Assist

--- app/models/user/themes.rb
+++ app/models/user/themes.rb
@@ -5,10 +5,17 @@ class User
         included do
             field :web_theme, type: String
             attr_accessible :web_theme, as: :user
+
+            field :dyslexia_assist, type: Boolean
+            attr_accessible :dyslexia_assist, as: :user
         end
 
         def dark_theme?
             web_theme == "Dark Theme"
+        end
+
+        def dyslexia_assist?
+            dyslexia_assist == true
         end
     end
 end
