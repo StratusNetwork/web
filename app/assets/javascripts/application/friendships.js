2454e596e11eafa151e8f44162385696fd4918a5
Matic0B <Matic0B@users.noreply.github.com>
2017-12-14 13:51:49 +0100

    Various December Updates

--- app/assets/javascripts/application/friendships.js
+++ app/assets/javascripts/application/friendships.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-    $('#tabs a').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    })
-
     $(".friend-icon")
     .mouseenter(function () {
         $(this).find(".remove-friend").show();
