443a3b9e54aac9c348da3d44236f597fef74fde9
cswhite2000 <18whitechristop@gmail.com>
2018-01-20 19:55:52 -0800

    Use || instead of assignment

--- app/models/user/groups.rb
+++ app/models/user/groups.rb
@@ -14,7 +14,7 @@ class User
                         {
                             realm: realm,
                             text: "#{ChatColor[flair.color]}#{flair.symbol}",
-                            priority: flair.priority ||= group.priority,
+                            priority: flair.priority || group.priority,
                         }
                     end
                 end
