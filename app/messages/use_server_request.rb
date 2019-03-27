a994c5a0a0719b5e137e9648844e2121c607eaab
Christopher White <cswhite2000@users.noreply.github.com>
2019-03-26 21:50:53 -0700

    Add private server requesting with custom names (#32)

--- app/messages/use_server_request.rb
+++ app/messages/use_server_request.rb
@@ -1,8 +1,13 @@
 # Request to use a certain server.
 class UseServerRequest < BaseMessage
     field :user_id
+    field :server_name
 
     def user
         User.need(user_id)
+    end
+
+    def name
+        (server_name.blank?) ? user.username : server_name
     end
 end
